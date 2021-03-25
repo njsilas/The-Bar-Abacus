e83186993d12c08650d86a347c827bf1c5e3af22
Nicholas Silas <njsilas@gmail.com>
2021-03-25 02:29:22 -0400

    displaying logic in cocktail class

--- src/components/CocktailsComponents/CocktailCalculator.js
+++ src/components/CocktailsComponents/CocktailCalculator.js
@@ -11,29 +11,67 @@ class CocktailCalculator extends Component {
 
 state = {
     name: "",
-    margin: 0,
     price: 0,
-    cocktail: this.props.cocktail
+    margin: 0
+}
+handleChange = e => {
+    const {name, value} = e.target
+  
+    this.setState({
+        [name]: value
+    })
+}
+priceChange = e => {
+    const {name, value} = e.target
+    this.setState({
+        [name]: parseFloat(value)
+        
+       })  
+}
+setMargin = e => {
+    e.preventDefault()
+    
+}
+marginForDisplay = (tot, price) => {
+    if(price === 0 || tot === 0) {
+        return "0%"
+    }else {
+        const roundednum = (tot / price) * 100
+        const addedper = roundednum.toFixed(2)+"%"
+        return addedper
+    }
+    
 }
     render() {
      // let liquorsList = [];
       //this.props.liquors.forEach(({id, brand, ppo }) => liquorsList.push({id, brand, ppo}))
       //let uniqueLiquor = [...new Set(liquorsList.map(liquor => liquor.id, liquor.brand, liquor.ppo ))]
       //let mixersList = []
-   let sum = this.props.cocktail.reduce(
+   let rawsum = this.props.cocktail.reduce(
     (accumulator, drink) => accumulator + drink.ozppo
     ,0
 )
+let sum = "$" + parseFloat(rawsum)
+let margin = this.marginForDisplay(sum, this.state.price)
+
+   
 console.log(this.state)
         return (
             <div>
                <CocktailLiquorForm />
                 <CocktailMixerForm />
-                <form>
-                    <label>Cocktail:</label>
+                <form onSubmit={this.setMargin}>
+                    <label>Cocktail Name:</label>
+                    <input type="text" name="name" onChange={this.handleChange}></input>
+                    <label>Potential Price</label>
+                    <input type="number" name="price" onChange={this.priceChange}></input>
+                    <input type="submit" value="check price"></input>
                 </form>
-                <CocktailDisplay cocktail={this.props.cocktail} />
-                {sum}
+                <CocktailDisplay cocktail={this.props.cocktail} margin={margin} sum={sum}/>
+                <h1>{this.state.name}</h1>
+             
+                <br></br>
+              
             </div>
         );
     }
--- src/components/CocktailsComponents/CocktailDisplay.js
+++ src/components/CocktailsComponents/CocktailDisplay.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const CocktailDisplay = ({cocktail}) => {
+const CocktailDisplay = (props) => {
+    const {cocktail, margin, sum} = props 
+    console.log(cocktail, margin)
     return (
         <div>
-           {cocktail.map(cocktail => <ul><li>{cocktail.oz} OZ of{cocktail.ingname} @ {cocktail.ppo}</li></ul>)}
+           {cocktail.map(cocktail => <ul><li>{cocktail.oz} OZ of {cocktail.ingname} @ {cocktail.ppo} = ${cocktail.ozppo}</li></ul>)}
+           Total{sum} Margin:{margin} 
         </div>
     );
 };
