6fab89f10200bd3e53928fe4af608a55271946b5
Nicholas Silas <njsilas@gmail.com>
2021-06-16 19:33:10 -0400

    removed testing tools for production

--- src/components/CocktailsComponents/CocktailDisplay.js
+++ src/components/CocktailsComponents/CocktailDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CocktailDisplay = (props) => {
     const {cocktail, margin, sum} = props 
-    console.log(cocktail, margin)
+    
     return (
         <div>
            {cocktail.map(cocktail => <ul><li>{cocktail.oz} OZ of {cocktail.ingname} @ {cocktail.ppo}/oz = ${cocktail.ozppo.toFixed(2)}</li></ul>)}
