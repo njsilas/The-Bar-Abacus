c252c9954a0f4d709064b1178e38ae584bad57e7
Nicholas Silas <njsilas@gmail.com>
2021-10-17 20:50:39 -0400

    adding foundation for delete button for mixers

--- src/components/MixersComponents/MixersList.js
+++ src/components/MixersComponents/MixersList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 const MixersList = ({ mixers }) => {
    
     return (
