e237f911cf6e681964f392c1b28803cde9a1aa3e
Nicholas Silas <njsilas@gmail.com>
2021-10-28 17:47:12 -0400

    button created, testing if it works

--- src/components/LiquorsComponents/LiquorsList.js
+++ src/components/LiquorsComponents/LiquorsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { removeLiq } from '../../actions/liquorsActions';
 import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 const LiquorsList = ({ liquors }) => {
    
     return (
         <div class="snameofstyleiwant">
-           {liquors.map(liquor => <ListGroup key="liquormap"><ListGroupItem key={liquor.id}> {liquor.brand} @ ${liquor.ppo}/oz</ListGroupItem> </ListGroup>)}
+           {liquors.map(liquor => <ListGroup key="liquormap"><ListGroupItem key={liquor.id}> {liquor.brand} @ ${liquor.ppo}/oz <Button onSubmit= {removeLiq(liquor)} ></Button></ListGroupItem> </ListGroup>)}
         </div>
     );
 };
